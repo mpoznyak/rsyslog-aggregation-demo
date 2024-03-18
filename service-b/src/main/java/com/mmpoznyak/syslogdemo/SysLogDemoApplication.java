ca1398b33d8f83e497be7388fa3d938fa85e26b9
Maxim Poznyak <mmpoznyak@gmail.com>
2024-03-18 16:32:06 +0300

    init

---
+++ service-b/src/main/java/com/mmpoznyak/syslogdemo/SysLogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mmpoznyak.syslogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysLogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysLogDemoApplication.class, args);
+    }
+
+}
